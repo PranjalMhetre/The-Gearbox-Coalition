0891c1ffd56c209c9b8e0206fae08720edd108e5
LavaWaffle <60158215+LavaWaffle@users.noreply.github.com>
2022-04-20 18:53:01 -0400

    initial commit

---
+++ components/marker.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { Modal, UnstyledButton } from '@mantine/core';
+import { useClickOutside } from '@mantine/hooks';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+
+export default function CustomMarker({ teamData }) {
+  const team = teamData;
+
+  const [opened, setOpened] = useState(false);
+  const ref = useClickOutside(() => setOpened(false));
+  return (
+    <>
+      <Modal 
+        ref={ref}
+        centered
+        overlayColor='#f3f0ff'
+        overlayOpacity={0.3}
+        overflow="outside"
+        size="lg"
+        opened={opened} 
+        onClose={() => setOpened(false)} 
+        title={`Team Number: ${team.fields.teamNumber}`}
+      >
+        <h1 className='text-2xl'>{`Address: ${team.fields.address}`}</h1>
+        <h1 className='text-2xl'>{`Travel Time: ${team.fields.travelTime} minutes`}</h1>
+        <h1 className='text-2xl'>{`Distance: ${team.fields.distance} miles`}</h1>
+        <h1 className='text-2xl font-bold'>{`Has Field: ${team.fields.hasField ? 'yes' : 'no'}`}</h1> 
+        <h1 className='text-2xl'>{`Has Business: ${team.fields.hasBusinessSide ? 'yes' : 'no'}`}</h1>
+        <h1 className='text-2xl text-center'>--- Comp Record ---</h1>
+        <p className='text-xl'>{documentToReactComponents(team.fields.compRecord)}</p>
+      </Modal>
+
+      <UnstyledButton onClick={() => setOpened(true)} className="bg-red-300 hover:bg-blue-300 transition-colors duration-300 w-[2rem] aspect-square rounded-[50%] inline-flex justify-center items-center text-white">
+        {team.fields.teamNumber}
+      </UnstyledButton>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,54 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import Map, { Marker } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import React from 'react'
+import useWindowSize from "../components/useWindow"
+import CustomMarker from "../components/marker"
+import { createClient } from 'contentful'
 
-export default function HomePage() {
+export async function getStaticProps(){
+  const client = createClient({
+    space: 'mcgbm1socg24', //process.env.CONTENTFUL_SPACE_ID,
+    accessToken: 'L6zPnIjA2mGQDQnCnPHk2QGEB-SczBtYRTO2ZMbQxWM', //process.env.CONTENTFUL_ACCESS_TOKEN,
+  })
+
+  const teamRes = await client.getEntries({
+    content_type: 'team',
+  })
+  return {
+    props: {
+      teams: teamRes.items,
+    },
+  }
+}
+
+export default function HomePage({ teams }) {
+  const { width, height } = useWindowSize()
+  console.log(teams)
+
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
-      <div className='text-green-300 dark:text-red-300 text-center mt-2 '>Light === green, dark === red</div>
+      <Map
+        initialViewState={{
+          longitude: -74.791030,
+          latitude: 40.328070,
+          zoom: 7
+        }}
+        style={{width, height}}
+        mapboxAccessToken="pk.eyJ1IjoibGF2YXdhZmZsZSIsImEiOiJjbDI4MnE2ZnMwNWZvM2xvMW96eDdndXc4In0.hpG9-aeBSEHWFmfwqYObkw"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        {teams && teams.map(team => {
+          return (
+          <Marker key={team.sys.id} longitude={team.fields.longitude} latitude={team.fields.latitude} anchor="center" >
+            <CustomMarker teamData={team} />
+          </Marker>
+          )
+        })}
+
+        {/* <Marker longitude={-74.4757} latitude={40.6520} anchor="center" >
+          <CustomMarker />
+        </Marker> */}
+      </Map>
     </>
   );
 }
